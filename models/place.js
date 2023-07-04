6102d09e012536b41af3b0c1c56a10a60a9ae27d
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-07-04 12:36:11 +0500

    optimizing code

--- models/place.js
+++ models/place.js
@@ -1,46 +1,19 @@
 import mongoose from "mongoose";
 
 const placeSchema = new mongoose.Schema({
-  place: {
-    place_id: {
-      type: String,
-      required: true,
-    },
-    name: {
-      type: String,
-      required: true
-    },
-    isOpenNow: {
-      type: Boolean,
-      required: true,
-    },
-    working_hours: { type: String, required: false },
-  },
-  fuel: {
-    fuel_types: [
-      {
-        id: {
-          type: String,
-          required: true,
-        },
-        name: {
-          type: String,
-          required: true,
-        },
-        price: {
-          type: String || Number,
-          required: true,
-        },
-        isAvailable: {
-          type: Boolean,
-          require: true
-        } 
-      }
-    ],
-  },
-  discount: {
+  place_id: {
     type: String,
-    required: false,
-  },  
+    required: true,
+  },
+  fuel_price: {
+    type: String,
+    required: true,
+  },
+  isOpenNow: {
+    type: String,
+    required: true,
+  },
+  working_hours: { type: String, required: false },
+  
 });
 export default mongoose.model("Place", placeSchema);
