fce36b05ecd7c164f231198a90e6ccd1184e4819
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-07-04 13:44:27 +0500

    edit feature

--- routes/places.js
+++ routes/places.js
@@ -5,7 +5,9 @@ import "dotenv/config.js";
 const router = express.Router();
 
 router.post("/add", async (req, res) => {
-  const { place_id, fuel_price, isOpenNow, working_hours } = req.body;
+  const { place: { place_id, isOpenNow, name, working_hours }, fuel: {
+    fuel_types
+  }, discount  } = req.body;
   try {
     const existingPlace = await Place.findOne({ place_id });
 
@@ -15,12 +17,19 @@ router.post("/add", async (req, res) => {
         .json({ status: "error", error: "Bu joy allaqachon mavjud" });
     }
 
-    const newPlace = Place.create({
-      place_id,
-      fuel_price,
-      isOpenNow,
-      working_hours,
+    const newPlace = await Place.create({
+      place: {
+        place_id,
+        isOpenNow,
+        name,
+        working_hours
+      },
+      fuel: {
+        fuel_types
+      },
+      discount
     });
+
     res.status(201).json({ new_place: newPlace });
   } catch (error) {
     console.log(error);
@@ -32,6 +41,35 @@ router.get("/", async (req, res) => {
   try {
     const places = await Place.find({});
     res.json({ status: "ok", data: places });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.put("/edit/:id", async (req, res) => {
+  const { place: { place_id, isOpenNow, name, working_hours }, fuel: {
+    fuel_types
+  }, discount } = req.body;
+
+  try {
+    const existingPlace = await Place.findById(place_id);
+
+    if (!existingPlace) {
+      return res
+        .status(404)
+        .json({ status: "error", error: "Place not found" });
+    }
+
+    existingPlace.place.place_id = place_id;
+    existingPlace.fuel.fuel_types = fuel_types;
+    existingPlace.place.isOpenNow = isOpenNow;
+    existingPlace.place.working_hours = working_hours;
+    existingPlace.discount = discount
+
+    const updatedPlace = await existingPlace.save();
+
+    res.json({ status: "ok", updated_place: updatedPlace });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Server error" });
